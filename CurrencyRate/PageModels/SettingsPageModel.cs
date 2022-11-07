8f3830004a19e7a534f4c273cee689ee9808b940
Nikolay <d8putat@gmail.com>
2022-11-07 16:18:33 +0300

    Adding Setting Page
    Setting Page with Header and Buttons ( back and save )

---
+++ CurrencyRate/PageModels/SettingsPageModel.cs
@@ -0,0 +1,20 @@
+﻿using CurrencyRate.Helpers;
+using FreshMvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CurrencyRate.PageModels
+{
+    public class SettingsPageModel : FreshBasePageModel
+    {
+        public ICommand BackToMainPageCommand => SingleExecutionCommand.FromFunc(BackToMainPage);
+        public SettingsPageModel() { }
+        private async Task BackToMainPage()
+        {
+            await CoreMethods.PopPageModel();
+        }
+    }
+}
